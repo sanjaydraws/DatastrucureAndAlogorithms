517af61c4c1a46ffde7bfc3e2ed72517cc69a0a9
sanjaydraws <sprajapat8331@gmail.com>
2021-11-05 22:58:18 +0530

    reverse string

---
+++ recursion/Program9.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<vector> 
+
+#include<string> 
+using namespace std;
+
+class Solution {
+public:
+	void rev(vector<char>& str, int l, int r){
+		if(l >= r){
+			return;
+		}
+		swap(str[l++], str[r--]);                            
+		rev(str, l, r);
+	}
+	void reverseString(vector<char>& s) {
+		rev(s, 0, s.size() - 1);
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+    std::vector<char> input({ 'a', 'b', 'c' });
+    Solution obj1 ; 
+    obj1.reverseString(input);
+
+
+    return 0;
+}
